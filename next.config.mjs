c366ba2518985671fffa86a8d6b3b00775c75895
Julius Haas <juliushaas91@gmail.com>
2025-05-10 15:55:17 +0300

    feat: add taskfile and exclude devIndicators

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
     NEXT_PUBLIC_X_TENANT_ID: process.env.NEXT_PUBLIC_X_TENANT_ID,
     NEXT_PUBLIC_ACCESS_CODE: process.env.NEXT_PUBLIC_ACCESS_CODE,
   },
+  devIndicators: false
 };
 
 export default nextConfig;
---
+++ taskfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Tasks
+run() {
+  pnpm dev
+}
+
+# Check if the provided argument matches any of the functions
+if declare -f "$1" > /dev/null; then
+  "$@"  # If the function exists, run it with any additional arguments
+else
+  echo "Error: Unknown task '$1'"
+  echo
+  help  # Show help if the task is not recognized
+fi
+
+# Run application if no argument is provided
+"${@:-run}"
